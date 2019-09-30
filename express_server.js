38106dde264c92a24063a2d1c2ca626cfe7fa6dd
Tammie Chung <tammie.chung11@gmail.com>
2019-09-30 20:39:57 +0000

    use body-parser to be able to use POST requests easily and generated a random string to serve as shortURL

--- express_server.js
+++ express_server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
+app.use((bodyParser.urlencoded({extended: true})));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const generateRandomString = function() {
+  return Math.random().toString(36).substr(2, 6);
+};
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -25,6 +32,15 @@ app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+})
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
+})
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
