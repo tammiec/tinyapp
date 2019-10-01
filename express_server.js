b97acc7c6a9fcbf46ecf52975940e72c3657bc8e
Tammie Chung <tammie.chung11@gmail.com>
2019-10-01 16:22:27 +0000

    display username next to form in header when logged in

--- express_server.js
+++ express_server.js
@@ -37,13 +37,14 @@ app.post('/login', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: req.cookies.username };
   res.render('urls_index', templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
-})
+  let templateVars = { username: req.cookies.username }
+  res.render('urls_new', templateVars);
+});
 
 app.post('/urls', (req, res) => {
   console.log(req.body);
@@ -53,7 +54,7 @@ app.post('/urls', (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies.username };
   res.render("urls_show", templateVars);
 });
 
