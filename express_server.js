8eb93aca6a4da78e45471a24ccecdf886595ea45
Tammie Chung <tammie.chung11@gmail.com>
2019-10-03 02:02:03 +0000

    implemented analytics for unique visitors, total visitors, and visitor history

--- express_server.js
+++ express_server.js
@@ -3,16 +3,19 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
 const methodOverride = require('method-override');
 const { generateRandomString, getUserByEmail, urlsForUser } = require('./helpers');
+const { URL, User } = require('./classes');
 
 app.set('view engine', 'ejs');
 app.use((bodyParser.urlencoded({extended: true})));
+app.use(cookieParser());
 app.use(cookieSession({
   name: 'session',
-  keys: ['user_id']
+  keys: ['user_id', 'visitor']
 }));
 app.use(methodOverride('_method'));
 
@@ -103,11 +106,7 @@ app.post('/register', (req, res) => {
     res.status(400).send('This user already exists. Please <a href="/login">log in</a> or <a href="/register">create a new account</a>.');
   } else {
     let newId = generateRandomString();
-    users[newId] = {
-      id: newId,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, 10)
-    };
+    users[newId] = new User(newId, req.body.email, req.body.password);
     req.session.user_id = newId;
     res.redirect('/urls');
   }
@@ -126,10 +125,7 @@ app.get('/urls/new', (req, res) => {
 // creates a new shortURL
 app.post('/urls', (req, res) => {
   let newId = generateRandomString();
-  urlDatabase[newId] = {
-    longURL: req.body.longURL,
-    userID: req.session.user_id,
-  };
+  urlDatabase[newId] = new URL(req.body.longURL, req.session.user_id);
   res.redirect(`/urls/${newId}`);
 });
 
@@ -138,8 +134,14 @@ app.get("/urls/:shortURL", (req, res) => {
   if (!urlDatabase[req.params.shortURL]) {
     res.send('This shortURL does not exist. Click <a href="/urls">here</a> to return to homepage.');
   } else {
-    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: users[req.session.user_id], uniqueVisitors: urlDatabase[req.params.shortURL].uniqueVisitors, totalVisitors: urlDatabase[req.params.shortURL].totalVisitors };
-    console.log(templateVars);
+    let templateVars = { 
+      shortURL: req.params.shortURL, 
+      longURL: urlDatabase[req.params.shortURL].longURL, 
+      user: users[req.session.user_id], 
+      uniqueVisitors: urlDatabase[req.params.shortURL].uniqueVisitors, 
+      totalVisitors: urlDatabase[req.params.shortURL].totalVisitors,
+      visitHistory: urlDatabase[req.params.shortURL].visitHistory
+    };
     if (!templateVars.user || templateVars.user.id !== urlDatabase[req.params.shortURL].userID) {
       res.render('urls_denied', templateVars);
     } else {
@@ -165,9 +167,16 @@ app.get("/u/:shortURL", (req, res) => {
     res.send('This shortURL does not exist. Click <a href="/urls">here</a> to return to homepage.');
   } else {
     const longURL = urlDatabase[req.params.shortURL].longURL;
-    urlDatabase[req.params.shortURL].totalVisitors += 1;
-    urlDatabase[req.params.shortURL].uniqueVisitors += 1;
-    res.redirect(longURL);
+    let visitorID;
+    if (!req.cookies.visitor) {
+      visitorID = generateRandomString();
+    } else {
+      visitorID = req.cookies.visitor;
+    }
+    urlDatabase[req.params.shortURL].countTotalVisitors();
+    urlDatabase[req.params.shortURL].countUniqueVisitors(visitorID);
+    urlDatabase[req.params.shortURL].addVisitHistory(visitorID);
+    res.cookie('visitor', visitorID).redirect(longURL);
   }
 });
 
